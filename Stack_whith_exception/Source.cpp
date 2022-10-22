38feb013868783ed8f255c9083ff900a90c42757
Mikey191 <desertigorb@gmail.com>
2022-10-22 11:44:46 +0300

    Добавьте файлы проекта.

---
+++ Stack_whith_exception/Source.cpp
@@ -0,0 +1,167 @@
+//Дана строка символов, содержащая(){}[] и другие символы.
+//Проверить есть ли в строке баланс открывающих и закрывающих скобок.
+//если баланса нет выдать исключение с помощью try catch
+//так же вызвать исключение, если входная строка имеет больше 50 символов
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct node
+{
+	char c;
+	node* next;
+};
+
+class stack
+{
+private:
+	node* top;
+public:
+	stack() : top{ nullptr } {}
+	~stack();
+	bool IsEmpty();
+	int GetCount();
+	void Add(char symb);
+	char Extract();
+	void Print();
+};
+
+void Balance(string str);
+void MaxSymb(string str)
+{
+	try
+	{
+		if (str.length() > 50)
+			throw "Error. String length more 50 symbols!";
+	}
+	catch (const char* c)
+	{
+		cout << c << endl;
+	}
+}
+
+int main()
+{
+	try
+	{
+		string str{ "(asdf)(asdfs)[dssfe]876427475476542e3ygdedhgvcregvyytr3tr3y4763r3" };
+		MaxSymb(str);//проверка на максимальное кол-во символов
+		Balance(str);//проверка на баланс скобок
+	}
+	catch (...)
+	{
+		cout << "Error!";
+	}
+
+	return 0;
+}
+
+stack::~stack()
+{
+	while (top != nullptr)
+	{
+		node* work = top;
+		top = top->next;
+		delete work;
+	}
+}
+
+bool stack::IsEmpty()//проверка на то, что список пуст
+{
+	return top == nullptr;
+}
+
+int stack::GetCount()
+{
+	int count{ 0 };
+	node* work = top;
+	while (work != nullptr)
+	{
+		count++;
+		work = work->next;
+	}
+
+	return count;
+}
+
+void stack::Add(char symb)
+{
+	node* work = new node;
+	work->next = top;
+	top = work;
+	work->c = symb;
+}
+
+char stack::Extract()
+{
+	if (top != nullptr)
+	{
+		node* work = top;
+		char symb = work->c;
+		top = top->next;
+		delete work;
+		return symb;
+	}
+	return -1;
+}
+
+void stack::Print()
+{
+	node* work{ top };
+	while (work != nullptr)
+	{
+		cout << work->c << " ";
+		work = work->next;
+	}cout << endl;
+}
+
+void Balance(string str)
+{
+	bool Bal;
+	stack st;
+	char temp;
+	char c1 = '(';
+	char c2 = ')';
+
+	char c3 = '{';
+	char c4 = '}';
+
+	char c5 = '[';
+	char c6 = ']';
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str.at(i) == c1 || str.at(i) == c3 || str.at(i) == c5)
+		{
+			st.Add(str.at(i));
+		}
+		else if (str.at(i) == c2 || str.at(i) == c4 || str.at(i) == c6)
+		{
+			if (st.IsEmpty()) Bal = false;
+			else
+			{
+				temp = st.Extract();
+				if (
+					temp == c1 && str.at(i) == c2 ||
+					temp == c3 && str.at(i) == c4 ||
+					temp == c5 && str.at(i) == c6
+					)Bal = true;
+				else
+				{
+					Bal = false;
+					break;
+				}
+			}
+		}
+	}
+	try
+	{
+		if (Bal && st.IsEmpty()) cout << "Good" << endl;//проверка на пустоту стека. Если он не пуст, то в строке были не закрытые скобки
+		else throw "Bad";
+	}
+	catch (const char* c)
+	{
+		cout << "String isn't have balance "<< c << endl;
+	}
+}
+
